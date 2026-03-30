6896a4b3472b3abcce7ce530c16172ca31ad9a56
Zk-nd3r <zk_nd3r@users.noreply.github.com>
2026-03-30 20:17:25 +0000

    add gitignore, remove build artifacts

--- target/debug/deps/zcash_memo_decode-228b562424a9f751.d
+++
@@ -1,6 +0,0 @@
-/opt/builds/zcash-memo-decode/target/debug/deps/zcash_memo_decode-228b562424a9f751.d: src/lib.rs src/tvlv.rs
-
-/opt/builds/zcash-memo-decode/target/debug/deps/zcash_memo_decode-228b562424a9f751: src/lib.rs src/tvlv.rs
-
-src/lib.rs:
-src/tvlv.rs:
--- target/debug/deps/zcash_memo_decode-431e2b5ebf56b55e.d
+++
@@ -1,8 +0,0 @@
-/opt/builds/zcash-memo-decode/target/debug/deps/zcash_memo_decode-431e2b5ebf56b55e.d: src/lib.rs src/tvlv.rs
-
-/opt/builds/zcash-memo-decode/target/debug/deps/libzcash_memo_decode-431e2b5ebf56b55e.rlib: src/lib.rs src/tvlv.rs
-
-/opt/builds/zcash-memo-decode/target/debug/deps/libzcash_memo_decode-431e2b5ebf56b55e.rmeta: src/lib.rs src/tvlv.rs
-
-src/lib.rs:
-src/tvlv.rs:
